7c595e12237ff43736eec22754b3d105c9ae65b6
Gigara <info@gigara.info>
2020-04-14 13:04:44 +0530

    Initial commit

---
+++ Backend/src/test/java/com/giga/FashionStore/GigaFashionStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giga.FashionStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigaFashionStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
